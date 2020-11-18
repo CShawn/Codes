fd2cd82a8bb68ca87310151ed51b1c1a073f1905
CShawn <cshawn1024x@gmail.com>
2020-11-18 22:43:14 +0800

    add: sword_10_1

---
+++ app/src/test/java/com/cshawn/leetcodes/sword/Sword_10_1Test.java
@@ -0,0 +1,20 @@
+package com.cshawn.leetcodes.sword;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2020/11/18 22:46
+ */
+public class Sword_10_1Test {
+    private Sword_10_1 test = new Sword_10_1();
+    @ParameterizedTest(name = "n = {0}, expected = {1}")
+    @CsvSource({"0, 0", "1, 1", "2, 1", "6, 8", "45, 134903163", "100, 687995182"})
+    public void fib(int n, int expected) {
+        assertEquals(expected, test.fib(n));
+    }
+}
