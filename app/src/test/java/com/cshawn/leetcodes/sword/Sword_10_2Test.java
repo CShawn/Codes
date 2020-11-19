396ba380c2ab021bb154ecfd3a8706494e8560a9
cshawn <cshawn1024x@gmail.com>
2020-11-19 18:29:16 +0800

    add: sword10_2

---
+++ app/src/test/java/com/cshawn/leetcodes/sword/Sword_10_2Test.java
@@ -0,0 +1,19 @@
+package com.cshawn.leetcodes.sword;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author C.Shawn
+ * @date 2020/11/19 18:17
+ */
+class Sword_10_2Test {
+    private Sword_10_2 test = new Sword_10_2();
+    @ParameterizedTest(name = "n = {0}, expected = {1}")
+    @CsvSource({"0, 0", "1, 1", "2, 2", "6, 13", "45, 836311896", "100, 782204094"})
+    void numWays(int n, int ways) {
+        assertEquals(ways, test.numWays(n));
+    }
+}
