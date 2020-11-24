cd49bde3fc4ac057934f802ef5d38cc6c7a7a5b1
CShawn <cshawn1024x@gmail.com>
2020-11-24 23:30:02 +0800

    <add>(ListNode): 添加getSize和toArray方法

--- app/src/main/java/com/cshawn/leetcodes/sword/ListNode.java
+++ app/src/main/java/com/cshawn/leetcodes/sword/ListNode.java
@@ -1,9 +1,15 @@
 package com.cshawn.leetcodes.sword;
 
+import java.util.Arrays;
+
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
+
+    ListNode(int x) {
+        val = x;
+    }
+
     ListNode next(int n) {
         findEnd(this).next = new ListNode(n);
         return this;
@@ -15,5 +21,36 @@ public class ListNode {
         } else {
             return node;
         }
+    }
+
+    public int getSize() {
+        ListNode ln = this;
+        int size = 0;
+        while (ln != null) {
+            size++;
+            ln = ln.next;
+        }
+        return size;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[getSize()];
+        ListNode ln = this;
+        int index = 0;
+        while (ln != null) {
+            array[index] = ln.val;
+            ln = ln.next;
+            index++;
+        }
+        return array;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null && obj instanceof ListNode) {
+            ListNode node = (ListNode) obj;
+            return Arrays.equals(this.toArray(), node.toArray());
+        }
+        return super.equals(obj);
     }
 }
