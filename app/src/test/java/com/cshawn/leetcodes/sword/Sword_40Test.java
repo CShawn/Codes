7ed7edda7d1d466a3b82ca178f35aff740361962
CShawn <cshawn1024x@gmail.com>
2021-01-09 20:54:10 +0800

    <modify>(Sword_40):修改单元测试

--- app/src/test/java/com/cshawn/leetcodes/sword/Sword_40Test.java
+++ app/src/test/java/com/cshawn/leetcodes/sword/Sword_40Test.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-
 import java.util.Arrays;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,14 +20,14 @@ class Sword_40Test {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
             return Stream.of(
-                    // Arguments.of(new int[0], 9, new int[0]),
-                    // Arguments.of(new int[]{1,2,3}, 0, new int[0]),
-                    // Arguments.of(new int[]{1,2,3}, 4, new int[]{1,2,3}),
-                    // Arguments.of(new int[]{3,2,1}, 1, new int[]{1}),
-                    // Arguments.of(new int[]{1,1,1}, 1, new int[]{1}),
-                    // Arguments.of(new int[]{3,2,1}, 2, new int[]{1,2}),
-                    // Arguments.of(new int[]{0,0,1,2,4,2,2,3,1,4}, 8, new int[]{0,0,1,1,2,2,2,3}),
-                    // Arguments.of(new int[]{0,0,1,3,4,5,0,7,6,7}, 9, new int[]{0,0,0,1,3,4,5,6,7}),
+                    Arguments.of(new int[0], 9, new int[0]),
+                    Arguments.of(new int[]{1,2,3}, 0, new int[0]),
+                    Arguments.of(new int[]{1,2,3}, 4, new int[]{1,2,3}),
+                    Arguments.of(new int[]{3,2,1}, 1, new int[]{1}),
+                    Arguments.of(new int[]{1,1,1}, 1, new int[]{1}),
+                    Arguments.of(new int[]{3,2,1}, 2, new int[]{1,2}),
+                    Arguments.of(new int[]{0,0,1,2,4,2,2,3,1,4}, 8, new int[]{0,0,1,1,2,2,2,3}),
+                    Arguments.of(new int[]{0,0,1,3,4,5,0,7,6,7}, 9, new int[]{0,0,0,1,3,4,5,6,7}),
                     Arguments.of(new int[]{0,1,1,2,4,4,1,3,3,2}, 6, new int[]{0,1,1,1,2,2})
 
             );
