7d8768b3f3ec5fc41f65f75df9fbbe9f552a80b8
cshawn <cshawn1024x@gmail.com>
2021-01-18 21:28:31 +0800

    <modify>(Sword_48): 完成48方法2

--- app/src/main/java/com/cshawn/leetcodes/sword/Sword_48.java
+++ app/src/main/java/com/cshawn/leetcodes/sword/Sword_48.java
@@ -1,5 +1,8 @@
 package com.cshawn.leetcodes.sword;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 最长不含重复字符的子字符串
  * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
@@ -47,6 +50,7 @@ public class Sword_48 {
                     left = i + 1;
                     // 标记有重复
                     repeat = true;
+                    break;
                 }
             }
             // 无重复时才计算最大子串长度，有重复必然比之前的max要小
@@ -60,6 +64,70 @@ public class Sword_48 {
             }
             // 右侧边界向右移动
             right++;
+        }
+        return max;
+    }
+
+    // 查找子串是否有重复时，可以使用HashMap来优化速度
+    public int lengthOfLongestSubstring2(String s) {
+        if (s == null) {
+            return 0;
+        }
+        // 定义当前不重复子串的两侧边界和当前最大子串的长度
+        int left = 0, right = 0, max = 0;
+        // 存储字符出现的位置,字符集最多256个
+        Map<Character, Integer> map = new HashMap<>(256);
+        // 遍历到右边界超出
+        while (right < s.length()) {
+            int index = map.getOrDefault(s.charAt(right), -1);
+            // 若有重复
+            if (index >= left) {
+                // 将左边界置为重复字符的后一个字符的位置
+                left = index + 1;
+            } else {
+                // 无重复时才计算最大子串长度，有重复必然比之前的max要小
+                // 当前子串长度
+                int length = right - left + 1;
+                // 更新最大值
+                if (length > max) {
+                    max = length;
+                }
+            }
+            map.put(s.charAt(right), right);
+            // 右侧边界向右移动
+            right++;
+        }
+        return max;
+    }
+
+    /**
+     * 动态规划
+     * dp[i]表示以s[i]结尾的最大不重复子串
+     * 查找s[i]是否出现过，出现位置记为j
+     * 如果index在i - dp[i - 1]之前，则dp[i] = dp[i - 1] + 1
+     * 如果在[i - dp[i], i]之间，则dp[i] = i - index
+     */
+    public int lengthOfLongestSubstring3(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        // 存储字符出现的位置,字符集最多256个
+        Map<Character, Integer> map = new HashMap<>(256);
+        int[] dp = new int[s.length()];
+        dp[0] = 1;
+        map.put(s.charAt(0), 0);
+        int max = 1;
+        for (int i = 1; i < s.length(); i++) {
+            int index = map.getOrDefault(s.charAt(i), -1);
+            if (index < i - dp[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = i - index;
+            }
+            map.put(s.charAt(i), i);
+            if (dp[i] > max) {
+                max = dp[i];
+            }
         }
         return max;
     }
--- app/src/test/java/com/cshawn/leetcodes/sword/Sword_48Test.java
+++ app/src/test/java/com/cshawn/leetcodes/sword/Sword_48Test.java
@@ -15,4 +15,16 @@ class Sword_48Test {
     void lengthOfLongestSubstring(String str, int expected) {
         assertEquals(expected, new Sword_48().lengthOfLongestSubstring(str));
     }
+
+    @ParameterizedTest
+    @CsvSource({",0", "a,1", "ab,2", "aa,1", "aba,2", "abb,2", "abc,3", "abcddcabde,5"})
+    void lengthOfLongestSubstring2(String str, int expected) {
+        assertEquals(expected, new Sword_48().lengthOfLongestSubstring2(str));
+    }
+
+    @ParameterizedTest
+    @CsvSource({",0", "a,1", "ab,2", "aa,1", "aba,2", "abb,2", "abc,3", "abcddcabde,5"})
+    void lengthOfLongestSubstring3(String str, int expected) {
+        assertEquals(expected, new Sword_48().lengthOfLongestSubstring3(str));
+    }
 }
