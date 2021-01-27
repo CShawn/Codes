db548349c3fed7bced84bea4b124f900d6a20932
cshawn <cshawn1024x@gmail.com>
2021-01-27 21:06:34 +0800

    <add>(Sword_64): 完成64

---
+++ app/src/main/java/com/cshawn/leetcodes/sword/Sword_64.java
@@ -0,0 +1,27 @@
+package com.cshawn.leetcodes.sword;
+
+/**
+ * 求1+2+…+n
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ * 示例 1：
+ * 输入: n = 3
+ * 输出: 6
+ *
+ * 示例 2：
+ * 输入: n = 9
+ * 输出: 45
+ * 限制：1 <= n <= 10000
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author C.Shawn
+ * @date 2021/1/27 8:39 下午
+ */
+public class Sword_64 {
+    public int sumNums(int n) {
+        int sum = n;
+        // 利用逻辑短路来充当if判断n--到0在，后半部分>0的条件无意义
+        boolean flag = n > 0 && (sum += sumNums(n - 1)) > 0;
+        return sum;
+    }
+}
