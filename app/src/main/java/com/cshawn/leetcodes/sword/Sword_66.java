90806be2c5938a7943325295a86f7cd96780b080
cshawn <cshawn1024x@gmail.com>
2021-01-28 10:39:04 +0800

    <add>(Sword_66): 完成66

---
+++ app/src/main/java/com/cshawn/leetcodes/sword/Sword_66.java
@@ -0,0 +1,37 @@
+package com.cshawn.leetcodes.sword;
+
+/**
+ * 构建乘积数组
+ * 给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中B[i] 的值是数组 A 中除了下标 i 以外的元素的积, 即B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+ *
+ * 示例:
+ * 输入: [1,2,3,4,5]
+ * 输出: [120,60,40,30,24]
+ * 提示：所有元素乘积之和不会溢出 32 位整数，a.length <= 100000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/gou-jian-cheng-ji-shu-zu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author C.Shawn
+ * @date 2021/1/28 10:11 上午
+ */
+public class Sword_66 {
+    public int[] constructArr(int[] a) {
+        if(a.length == 0) {
+            return new int[0];
+        }
+        int[] b = new int[a.length];
+        b[0] = 1;
+        // 先将当前索引之前的数字相乘
+        for (int i = 1; i < a.length; i++) {
+            b[i] = b[i - 1] * a[i - 1];
+        }
+        // 再倒着将当前索引之后的数字相乘
+        int temp = 1;
+        for (int i = a.length - 2; i >= 0; i--) {
+            temp *= a[i + 1];
+            b[i] *= temp;
+        }
+        return b;
+    }
+}
