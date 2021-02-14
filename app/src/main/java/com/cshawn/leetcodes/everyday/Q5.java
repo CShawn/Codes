f4d75be818b4b02a62fb75844dbd10b264f092c8
cshawn <cshawn1024x@gmail.com>
2021-02-14 23:37:53 +0800

    <add>(Q5): 完成5方法2

--- app/src/main/java/com/cshawn/leetcodes/everyday/Q5.java
+++ app/src/main/java/com/cshawn/leetcodes/everyday/Q5.java
@@ -33,7 +33,7 @@ package com.cshawn.leetcodes.everyday;
  */
 public class Q5 {
     // 方法1：动态规划 [i,j]区间字符串回文字符串长度，dp[i][j]=dp[i+1][j-1]>0?dp[i+1][j-1]+2:0
-    public String longestPalindrome(String s) {
+    public String longestPalindrome1(String s) {
         if (s == null || s.length() == 0) {
             return s;
         }
@@ -52,7 +52,7 @@ public class Q5 {
                     boolean b = s.charAt(i) == s.charAt(j);
                     if (i + 1 > j - 1) {
                         // 第2个字符和倒数第二个字符越界，则只取决于首尾字符
-                        dp[i][j] = b ? 2: 1;
+                        dp[i][j] = b ? 2 : 1;
                     } else {
                         // 首尾字符相同且i+1到j-1为回文则当前范围回文长度+2
                         dp[i][j] = b && dp[i + 1][j - 1] == j - i - 1 ? dp[i + 1][j - 1] + 2 : dp[i + 1][j - 1];
@@ -63,6 +63,58 @@ public class Q5 {
                     count = dp[i][j];
                     result = s.substring(i, j + 1);
                 }
+            }
+        }
+        return result;
+    }
+
+    // 方法2：Manacher
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        // 当前回文中心位置与当前最大回文字符串的右边界位置0123456,*1*3*5*7*9*
+        int center = -1, right = -1;
+        // 构造manacher数组，为每个字符之间及字符串首尾添加*
+        char[] manacher = new char[s.length() * 2 + 1];
+        for (int i = 0; i < manacher.length; i++) {
+            manacher[i] = (i & 1) == 0 ? '*' : s.charAt((i - 1) >> 1);
+        }
+        // 记录以每个位置为中心的回文字符串半径
+        int[] radius = new int[manacher.length];
+        // 最大回文字符串半径,一个字符时为1
+        int maxRadius = 0;
+        String result = "";
+        for (int i = 0; i < manacher.length; i++) {
+            radius[i] = i >= right ? 1 : Math.min(radius[2 * center - i], right - i);
+            // 从i加当前回文半径开始探求是否有更大回文半径
+            while (i + radius[i] < manacher.length && i - radius[i] >= 0 && manacher[i + radius[i]] == manacher[i - radius[i]]) {
+                radius[i]++;
+            }
+            // 当前回文范围扩大，更新右边界与中文中心
+            if (i + radius[i] > right) {
+                right = i + radius[i] - 1;
+                center = i;
+            }
+            // 回文半径扩大
+            if (radius[i] > maxRadius) {
+                if ((i & 1) == 0) {
+                    // 偶数时且回文半径>1
+                    if (radius[i] > 1) {
+                        int index = i >> 1;
+                        int trueRadius = radius[i] >> 1;
+                        // 获取当前回文字符串
+                        result = s.substring(index - trueRadius, index + trueRadius);
+                    }
+                } else {
+                    // 奇数时
+                    int index = (i - 1) >> 1;
+                    int trueRadius = (radius[i] - 1) >> 1;
+                    // 获取当前回文字符串
+                    result = s.substring(index - trueRadius, index + trueRadius + 1);
+                }
+                // 更新最大回文半径
+                maxRadius = radius[i];
             }
         }
         return result;
--- app/src/test/java/com/cshawn/leetcodes/everyday/Q5Test.java
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q5Test.java
@@ -11,7 +11,35 @@ import org.junit.jupiter.params.provider.CsvSource;
 class Q5Test {
 
     @ParameterizedTest
-    @CsvSource({",", "'',''", "a,a", "ab,b", "aa,aa", "aba,aba", "babad,aba", "cbbasdbbbad,bbb", "cbbasdbbbadabbbwesdwewasdasfsadssaas,bbbadabbb"})
+    @CsvSource({
+            ",",
+            "'',''",
+            "a,a",
+            "ab,b",
+            "aa,aa",
+            "aba,aba",
+            "babad,aba",
+            "cbbasdbbbad,bbb",
+            "cbbasdbbbadabbbwesdwewasdasfsadssaas,sdasfsads",
+            "babadada,adada"
+    })
+    void longestPalindrome1(String s, String result) {
+        Assertions.assertEquals(result, new Q5().longestPalindrome1(s));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+//            ",",
+//            "'',''",
+//            "a,a",
+//            "ab,a",
+//            "aa,aa",
+//            "aba,aba",
+//            "babad,bab",
+//            "cbbasdbbbad,bbb",
+//            "cbbasdbbbadabbbwesdwewasdasfsadssaas,bbbadabbb",
+            "babadada,adada"
+    })
     void longestPalindrome(String s, String result) {
         Assertions.assertEquals(result, new Q5().longestPalindrome(s));
     }
