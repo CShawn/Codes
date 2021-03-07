90a7e6e56bd6d6d9fccfa374583b0307a36ee080
cshawn <cshawn1024x@gmail.com>
2021-03-07 21:58:55 +0800

    <add>(Q131): 完成131

--- app/src/test/java/com/cshawn/leetcodes/everyday/Q131Test.java
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q131Test.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-
 import java.util.List;
 import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
