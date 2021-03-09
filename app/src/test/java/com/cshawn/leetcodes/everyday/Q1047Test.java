7da14c99a3afe2981e4f1414f4e4a55b8625f2fb
cshawn <cshawn1024x@gmail.com>
2021-03-09 20:51:04 +0800

    <add>(Q1047): 完成1047

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q1047Test.java
@@ -0,0 +1,19 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/3/9 7:13 下午
+ */
+class Q1047Test {
+
+    @ParameterizedTest
+    @CsvSource({",", "'',''", "a,a", "ab,ab", "aab,b", "aabb,''", "abb,a", "aba,aba", "abba,''", "abbbaac,abc", "abbacaasdasssssdddfaasddddkkkddssasasassa,csdasdfskasas"})
+    void removeDuplicates(String s, String result) {
+        Assertions.assertEquals(result, new Q1047().removeDuplicates1(s));
+        Assertions.assertEquals(result, new Q1047().removeDuplicates(s));
+    }
+}
