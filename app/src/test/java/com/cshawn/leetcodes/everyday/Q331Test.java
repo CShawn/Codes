75eacf5aceb43e8b4e77af1c7407fae73d081f07
cshawn <cshawn1024x@gmail.com>
2021-03-12 16:19:58 +0800

    <add>(Q331): 完成331

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q331Test.java
@@ -0,0 +1,28 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/3/12 3:28 下午
+ */
+class Q331Test {
+
+    @ParameterizedTest
+    @CsvSource(delimiterString = ";", value = {
+            ";false",
+            "'';false",
+            "1;false",
+            "9,3,4,#,#,1,#,#,2,#,6,#,#;true",
+            "1,#;false",
+            "9,#,#,1;false"
+    })
+    void isValidSerialization1(String s, boolean result) {
+        Assertions.assertEquals(result, new Q331().isValidSerialization1(s));
+        Assertions.assertEquals(result, new Q331().isValidSerialization2(s));
+        Assertions.assertEquals(result, new Q331().isValidSerialization3(s));
+        Assertions.assertEquals(result, new Q331().isValidSerialization4(s));
+    }
+}
