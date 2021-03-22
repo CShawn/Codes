58a57032e368c524b054961e7a7b60619cd3baa5
cshawn <cshawn1024x@gmail.com>
2021-03-22 17:37:54 +0800

    <add>(Q191): 完成191

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q191Test.java
@@ -0,0 +1,17 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/3/22 5:23 下午
+ */
+class Q191Test {
+    @ParameterizedTest
+    @CsvSource({"11,3", "128,1", "-1,32"})
+    void hammingWeight(int n, int result) {
+        Assertions.assertEquals(result, new Q191().hammingWeight(n));
+    }
+}
