ddc1ddee20191139e5839a2127950e3246b2563d
cshawn <cshawn1024x@gmail.com>
2021-04-04 13:35:22 +0800

    <add>(Q5706): 完成5706

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q5706Test.java
@@ -0,0 +1,24 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/4/3 11:59 下午
+ */
+class Q5706Test {
+    @ParameterizedTest
+    @CsvSource({
+            "My name is Haley,My Haley,true",
+            "of,A lot of words,false",
+            "a b c,a,true",
+            "a b c,c,true",
+            "a b c,d,false"
+    })
+    void areSentencesSimilar(String s1, String s2, boolean result) {
+        Assertions.assertEquals(result, new Q5706().areSentencesSimilar1(s1, s2));
+        Assertions.assertEquals(result, new Q5706().areSentencesSimilar(s1, s2));
+    }
+}
