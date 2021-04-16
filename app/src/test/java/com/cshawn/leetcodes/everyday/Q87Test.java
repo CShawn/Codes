00de58d9c690ef68a62a13bf2fae5dce53576e0e
cshawn <cshawn1024x@gmail.com>
2021-04-16 22:00:37 +0800

    <add>(Q87): 完成87

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q87Test.java
@@ -0,0 +1,18 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/4/16 9:49 下午
+ */
+class Q87Test {
+
+    @ParameterizedTest
+    @CsvSource({"great,rgeat,true", "abcde,caebd,false", "a,a,true", "a,b,false"})
+    void isScramble(String s1, String s2, boolean result) {
+        Assertions.assertEquals(result, new Q87().isScramble(s1, s2));
+    }
+}
