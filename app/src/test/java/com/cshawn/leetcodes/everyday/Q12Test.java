7d1652052bb12034f96bec1ee9778728cda5b0e9
cshawn <cshawn1024x@gmail.com>
2021-05-14 22:02:49 +0800

    <add>(Q12): 完成12

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q12Test.java
@@ -0,0 +1,24 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/5/14 9:11 下午
+ */
+class Q12Test {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,I", "2,II", "3,III", "4,IV", "5,V",
+            "6,VI", "8,VIII", "9,IX", "11,XI", "34,XXXIV", "49,XLIX",
+            "58,LVIII", "64,LXIV", "1994,MCMXCIV", "2021,MMXXI", "3900,MMMCM",
+            "3001,MMMI", "409,CDIX", "794,DCCXCIV"
+    })
+    void intToRoman(int num, String result) {
+        Assertions.assertEquals(result, new Q12().intToRoman1(num));
+        Assertions.assertEquals(result, new Q12().intToRoman(num));
+    }
+}
