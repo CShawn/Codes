be33792c8974005f8b3f0f745764dc254ff1d24b
cshawn <cshawn1024x@gmail.com>
2021-05-17 20:56:17 +0800

    <add>(Q993): 完成993

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q993Test.java
@@ -0,0 +1,35 @@
+package com.cshawn.leetcodes.everyday;
+
+import com.cshawn.leetcodes.sword.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
+
+/**
+ * @author C.Shawn
+ * @date 2021/5/17 5:25 下午
+ */
+class Q993Test {
+    static class DataArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.of(new TreeNode(new Integer[]{1,2,3,4}), 4, 3, false),
+                    Arguments.of(new TreeNode(new Integer[]{1,2,3,null,4}), 2, 3, false),
+                    Arguments.of(new TreeNode(new Integer[]{1,2,3,null,4,null,5}), 5, 4, true)
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DataArgumentsProvider.class)
+    void isCousins(TreeNode root, int x, int y, boolean result) {
+        Assertions.assertEquals(result, new Q993().isCousins(root, x, y));
+    }
+}
