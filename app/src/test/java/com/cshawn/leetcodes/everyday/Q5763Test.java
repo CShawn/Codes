de43aacbb17eb99e05075de4ed3cee1c78a71e21
cshawn <cshawn1024x@gmail.com>
2021-05-23 12:12:39 +0800

    <add>(Q5763): 完成5763

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q5763Test.java
@@ -0,0 +1,18 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/5/23 12:11 下午
+ */
+class Q5763Test {
+
+    @ParameterizedTest
+    @CsvSource({"1101, true", "111000, false", "110100010, false"})
+    void checkZeroOnes(String s, boolean result) {
+        Assertions.assertEquals(result, new Q5763().checkZeroOnes(s));
+    }
+}
