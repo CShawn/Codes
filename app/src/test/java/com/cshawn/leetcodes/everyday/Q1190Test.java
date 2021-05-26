ebc5d5c09cb64f04dd2d1a58fcb6f5db98951f3d
cshawn <cshawn1024x@gmail.com>
2021-05-26 22:28:56 +0800

    <add>(Q1190): 完成1190

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q1190Test.java
@@ -0,0 +1,18 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * @author C.Shawn
+ * @date 2021/5/26 10:22 下午
+ */
+class Q1190Test {
+
+    @ParameterizedTest
+    @CsvSource({"'',''", "(abcd),dcba", "(u(love)i),iloveu", "(ed(et(oc))el),leetcode", "a(bcdefghijkl(mno)p)q,apmnolkjihgfedcbq", "((ab)c(d)e),edcab"})
+    void reverseParentheses(String s, String result) {
+        Assertions.assertEquals(result, new Q1190().reverseParentheses(s));
+    }
+}
