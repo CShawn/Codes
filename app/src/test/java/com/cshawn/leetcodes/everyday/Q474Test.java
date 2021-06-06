9e5b8d1d1d4f0d17c1d8d3fb0470e0a7d5a06d19
CShawn <cshawn1024x@gmail.com>
2021-06-06 22:50:49 +0800

    <add>(Q474): 完成474

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q474Test.java
@@ -0,0 +1,34 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import java.util.stream.Stream;
+
+/**
+ * @author C.Shawn
+ * @date 2021/6/6 22:16
+ */
+public class Q474Test {
+    static class DataArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.arguments(new String[0], 2, 4, 0),
+                    Arguments.arguments(new String[]{"10", "0001", "111001", "1", "0"}, 5, 3, 4),
+                    Arguments.arguments(new String[]{"10", "0", "1"}, 1, 1, 2)
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DataArgumentsProvider.class)
+    int findMaxForm(String[] strs, int m, int n, int result) {
+        Assertions.assertEquals(result, new Q474().findMaxForm1(strs, m, n));
+        Assertions.assertEquals(result, new Q474().findMaxForm2(strs, m, n));
+        Assertions.assertEquals(result, new Q474().findMaxForm(strs, m, n));
+    }
+}
