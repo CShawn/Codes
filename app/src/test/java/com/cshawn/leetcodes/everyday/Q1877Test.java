1ae01201a80c842663ef764a5c6ce1dfb7bb06e7
cshawn <cshawn1024x@gmail.com>
2021-07-20 09:04:54 +0800

    <add>(Q1877): 完成1877

---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q1877Test.java
@@ -0,0 +1,31 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import java.util.stream.Stream;
+
+/**
+ * @author C.Shawn
+ * @date 2021/7/20 9:01 上午
+ */
+class Q1877Test {
+    static class DataArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.of(new int[]{3,5,2,3}, 7),
+                    Arguments.of(new int[]{3,5,4,2,4,6}, 8)
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DataArgumentsProvider.class)
+    void minPairSum(int[] nums, int result) {
+        Assertions.assertEquals(result,  new Q1877().minPairSum(nums));
+    }
+}
