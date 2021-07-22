b2d88c06a4961b5d6a5e0e21005c26cf40b70dac
cshawn <cshawn1024x@gmail.com>
2021-07-22 09:52:02 +0800

    <add>(Q138): 完成138

---
+++ app/src/main/java/com/cshawn/leetcodes/everyday/Q138.java
@@ -0,0 +1,142 @@
+package com.cshawn.leetcodes.everyday;
+
+import com.cshawn.leetcodes.sword.Sword_35;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 复制带随机指针的链表
+ * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ * 构造这个链表的深拷贝。深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。
+ * 新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。
+ * 复制链表中的指针都不应指向原链表中的节点 。
+ * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+ * 返回复制链表的头节点。
+ * 用一个由n个节点组成的链表来表示输入/输出中的链表。每个节点用一个[val, random_index]表示：
+ * val：一个表示Node.val的整数。
+ * random_index：随机指针指向的节点索引（范围从0到n-1）；如果不指向任何节点，则为null。
+ * 你的代码 只 接受原链表的头节点 head 作为传入参数。
+ *
+ * 示例 1：
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ *
+ * 示例 2：
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ *
+ * 示例 3：
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ *
+ * 示例 4：
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ *
+ * 提示：
+ * 0 <= n <= 1000
+ * -10000 <= Node.val <= 10000
+ * Node.random为空（null）或指向链表中的节点。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/copy-list-with-random-pointer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author C.Shawn
+ * @date 2021/7/22 8:44 上午
+ */
+public class Q138 {
+    // DFS + 哈希表
+    public Node copyRandomList1(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        return copy(head, map);
+    }
+
+    private Node copy(Node node, Map<Node, Node> map) {
+        if (node == null) {
+            return null;
+        }
+        Node n = map.get(node);
+        if (n == null) {
+            n = new Node(node.val);//.order(node.order);
+            map.put(node, n);
+            n.next = copy(node.next, map);
+            n.random = copy(node.random, map);
+        }
+        return n;
+    }
+
+    // 方法2：在每个结点后复制一个结点，赋值随机结点后再将链表拆分
+    public Node copyRandomList(Node head) {
+        // 在每个结点后复制一个结点
+        Node cur = head;
+        while (cur != null) {
+            Node cp = new Node(cur.val);//.order(cur.order);
+            cp.next = cur.next;
+            cur.next = cp;
+            cur = cp.next;
+        }
+        // 复制随机结点
+        cur = head;
+        while (cur != null) {
+            cur.next.random = cur.random == null ? null : cur.random.next;
+            cur = cur.next.next;
+        }
+        // 拆分链表
+        Node dummy = new Node(0);
+        Node result = dummy;
+        cur = head;
+        while (cur != null) {
+            result.next = cur.next;
+            result = result.next;
+            cur.next = cur.next.next;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+        private int order;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+
+        public Node order(int order) {
+            this.order = order;
+            return this;
+        }
+
+        public Node next(Node next) {
+            this.next = next;
+            return this;
+        }
+
+        public Node random(Node random) {
+            this.random = random;
+            return this;
+        }
+
+        public List<Integer[]> toArray() {
+            List<Integer[]> list = new ArrayList<>();
+            Node temp = this;
+            while (temp != null) {
+                list.add(getItem(temp));
+                temp = temp.next;
+            }
+            return list;
+        }
+
+        private Integer[] getItem(Node node) {
+            return new Integer[]{node.val, node.random != null ? node.random.order : null};
+        }
+    }
+}
---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q138Test.java
@@ -0,0 +1,52 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author C.Shawn
+ * @date 2021/7/22 9:07 上午
+ */
+class Q138Test {
+    private final Q138 test = new Q138();
+
+    static class NodeArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            Q138.Node one = new Q138.Node(1);
+            Q138.Node one2 = new Q138.Node(1).order(4);
+            Q138.Node three = new Q138.Node(3).random(one);
+            Q138.Node seven = new Q138.Node(7);
+            Q138.Node eleven = new Q138.Node(11);
+            return Stream.of(
+//                    Arguments.of(null, new Integer[0][0]),
+//                    Arguments.of(new Q138.Node(1), new Integer[][] {new Integer[]{1, null}}),
+                    Arguments.of(one.next(new Q138.Node(2).order(1).next(three.order(2))).random(three), new Integer[][] {new Integer[]{1, 2}, new Integer[]{2, null}, new Integer[]{3, 0}}),
+                    Arguments.of(
+                            seven.next(new Q138.Node(13).order(1).random(seven)
+                                    .next(eleven.order(2).random(one2)
+                                            .next(new Q138.Node(10).order(3).random(eleven)
+                                                    .next(one2.random(seven))))),
+                            new Integer[][]{{7,null},{13,0},{11,4},{10,2},{1,0}}
+                    )
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(NodeArgumentsProvider.class)
+    void copyRandomList(Q138.Node node, Integer[][] expected) {
+        if (node == null) {
+            assertEquals(0, expected.length);
+        } else {
+            assertArrayEquals(expected, test.copyRandomList1(node).toArray().toArray());
+            assertArrayEquals(expected, test.copyRandomList(node).toArray().toArray());
+        }
+    }
+}
