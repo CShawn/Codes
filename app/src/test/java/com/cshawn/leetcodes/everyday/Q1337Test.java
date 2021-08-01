940f73c9db12d0e3f543c5c4709e36949f5b86c6
cshawn <cshawn1024x@gmail.com>
2021-08-01 16:12:30 +0800

    <add>(Q1337): 完成1337

--- app/src/test/java/com/cshawn/leetcodes/everyday/Q1337Test.java
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q1337Test.java
@@ -1,40 +1,32 @@
 package com.cshawn.leetcodes.everyday;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-
-import java.util.Arrays;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author C.Shawn
- * @date 2021/6/19 10:29 上午
+ * @date 2021/8/1 4:08 下午
  */
-class Q1239Test {
+class Q1337Test {
     static class DataArgumentsProvider implements ArgumentsProvider {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
             return Stream.of(
-                    Arguments.of(new String[]{"un","iq","ue"}, 4),
-                    Arguments.of(new String[]{"cha","r","act","ers"}, 6),
-                    Arguments.of(new String[]{"abcdefghijklmnopqrstuvwxyz"}, 26),
-                    Arguments.of(new String[]{"aa", "b"}, 1),
-                    Arguments.of(new String[]{"aa", "bb"}, 0),
-                    Arguments.of(new String[]{"un","iq","ue", "wdsjkl", "sdk"}, 10)
+                    Arguments.of(new int[][]{{1,1,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,0,0,0},{1,1,1,1,1}}, 5, new int[]{2,0,3,1,4}),
+                    Arguments.of(new int[][]{{1,0,0,0},{1,1,1,1},{1,0,0,0},{1,0,0,0}}, 3, new int[]{0,2,3})
             );
         }
     }
 
     @ParameterizedTest
     @ArgumentsSource(DataArgumentsProvider.class)
-    void maxLength(String[] arr, int result) {
-        Assertions.assertEquals(result, new Q1239().maxLength(Arrays.asList(arr)));
+    void kWeakestRows(int[][] mat, int k, int[] result) {
+        Assertions.assertArrayEquals(result, new Q1337().kWeakestRows1(mat, k));
+        Assertions.assertArrayEquals(result, new Q1337().kWeakestRows(mat, k));
     }
 }
