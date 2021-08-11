aaf122c3c9e7b79e69457090870efbdfe55eb528
cshawn <cshawn1024x@gmail.com>
2021-08-11 12:50:02 +0800

    <add>(Q446): 完成446

---
+++ app/src/main/java/com/cshawn/leetcodes/everyday/Q446.java
@@ -0,0 +1,69 @@
+package com.cshawn.leetcodes.everyday;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 等差数列划分 II - 子序列
+ * 给你一个整数数组 nums ，返回 nums 中所有 等差子序列 的数目。
+ * 如果一个序列中 至少有三个元素 ，并且任意两个相邻元素之差相同，则称该序列为等差序列。
+ * 例如，[1, 3, 5, 7, 9]、[7, 7, 7, 7] 和 [3, -1, -5, -9] 都是等差序列。
+ * 再例如，[1, 1, 2, 5, 7] 不是等差序列。
+ * 数组中的子序列是从数组中删除一些元素（也可能不删除）得到的一个序列。
+ * 例如，[2,5,10] 是 [1,2,1,2,4,1,5,10] 的一个子序列。
+ * 题目数据保证答案是一个 32-bit 整数。
+ *
+ * 示例 1：
+ * 输入：nums = [2,4,6,8,10]
+ * 输出：7
+ * 解释：所有的等差子序列为：
+ * [2,4,6]
+ * [4,6,8]
+ * [6,8,10]
+ * [2,4,6,8]
+ * [4,6,8,10]
+ * [2,4,6,8,10]
+ * [2,6,10]
+ *
+ * 示例 2：
+ * 输入：nums = [7,7,7,7,7]
+ * 输出：16
+ * 解释：数组中的任意子序列都是等差子序列。
+ *
+ * 提示：
+ * 1 <= nums.length <= 1000
+ * -231 <= nums[i] <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/arithmetic-slices-ii-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author C.Shawn
+ * @date 2021/8/11 9:42 上午
+ */
+public class Q446 {
+    // 动态规划+哈希表
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums.length < 3) {
+            return 0;
+        }
+        // dp[i][j]表示以nums[i]结尾，公差为j的等差数列个数
+        Map<Long, Integer>[] dp = new Map[nums.length];
+        dp[0] = new HashMap<>();
+        dp[1] = new HashMap<>();
+        dp[1].put((long) nums[1] - (long) nums[0], 1);
+        int result = 0;
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = new HashMap<>();
+            for (int j = i - 1; j >= 0; j--) {
+                long diff = (long) nums[i] - (long) nums[j];
+                Map<Long, Integer> map = dp[j];
+                int count = map.getOrDefault(diff, 0);
+                dp[i].put(diff, dp[i].getOrDefault(diff, 0) + count + 1);
+                result += count;
+            }
+        }
+        return result;
+    }
+}
---
+++ app/src/test/java/com/cshawn/leetcodes/everyday/Q446Test.java
@@ -0,0 +1,35 @@
+package com.cshawn.leetcodes.everyday;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import java.util.stream.Stream;
+
+/**
+ * @author C.Shawn
+ * @date 2021/8/11 9:42 上午
+ */
+class Q446Test {
+    static class DataArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.of(new int[]{1,2,3,4}, 3),
+                    Arguments.of(new int[]{1,2,3}, 1),
+                    Arguments.of(new int[]{1}, 0),
+                    Arguments.of(new int[]{7,7,7,7,7}, 16),
+                    Arguments.of(new int[]{2,4,6,8,10}, 7),
+                    Arguments.of(new int[]{0,2000000000,-294967296}, 0)
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DataArgumentsProvider.class)
+    void numberOfArithmeticSlices(int[] nums, int result) {
+        Assertions.assertEquals(result, new Q446().numberOfArithmeticSlices(nums));
+    }
+}
